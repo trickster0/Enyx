5a22b7d4399e03b7daff150a091d775388a158ec
Thanasis Tserpelis <tserpthanasis@gmail.com>
2018-05-28 08:51:42 +0200

    Update enyx.py

--- enyx.py
+++ enyx.py
@@ -32,7 +32,6 @@ if os.path.isfile("/usr/bin/snmpwalk"):
 	IPS=output.split("\n")
 	output2 = len(IPS)-1
 	def ip(IPS):
-		global sentencecounter
 		for i in range(0,output2):
 			step1=IPS[i]
 			step2=''.join(step1)
@@ -52,12 +51,15 @@ if os.path.isfile("/usr/bin/snmpwalk"):
 			step7=''.join(step5)
 			step8=step7[:4] + ":" + step7[4:8] + ":" + step7[8:12] + ":" + step7[12:16] + ":" + step7[16:20] + ":" + step7[20:24] + ":" + step7[24:28] + ":" + step7[28:32]
 			sentences=["[+] Loopback -> ","[+] Link Local -> ","[+] Unique-Local -> "]
-			print sentences[sentencecounter] + step8
-			sentencecounter=sentencecounter+1
+			if step7[:4]=="fe80":
+				print sentences[1] + step8
+			elif step7[:4]=="0000":
+				print sentences[0] + step8
+			else:
+				print sentences[2] + step8
 
-        if output2 == 1:
-                print "[X] No IPv6 IP Found."
-        else:
-                print "[+] Grabbing IPv6."
-                sentencecounter=0
-                ip(IPS)
+	if output2 == 1:
+		print "[X] No IPv6 IP Found."
+	else:
+		print "[+] Grabbing IPv6."
+		ip(IPS)
